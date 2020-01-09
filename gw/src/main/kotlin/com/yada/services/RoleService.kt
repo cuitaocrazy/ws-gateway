80a9418235cacacb225d4e0570d8e55fbf258c8c
cuitao <cuitaocrazy@163.com>
2020-01-09 14:55:48 +0800

    init

--- gw/src/main/kotlin/com/yada/services/RoleService.kt
+++ gw/src/main/kotlin/com/yada/services/RoleService.kt
@@ -1,11 +1,13 @@
 package com.yada.services
 
+import com.yada.model.ResWithSvc
 import reactor.core.publisher.Mono
 
 interface IRoleService {
     fun create(appId: String, name: String): Mono<String?>
     fun changeName(id: String): Mono<Void>
-
+    fun changeResources(id: String, res: Array<ResWithSvc>): Mono<Void>
+    fun delete(id: String): Mono<Void>
 }
 
 abstract class RoleService : IRoleService {

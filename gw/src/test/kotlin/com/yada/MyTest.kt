31cce6a5466e36db71fcfabbd2264ccff113a52d
cuitao <cuitaocrazy@163.com>
2020-01-16 14:12:50 +0800

    修改一个null的bug

---
+++ gw/src/test/kotlin/com/yada/MyTest.kt
@@ -0,0 +1,23 @@
+package com.yada
+
+import com.yada.services.IAppService
+import com.yada.services.IOrgService
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.cloud.gateway.route.RouteLocator
+import java.lang.RuntimeException
+
+//@DataMongoTest
+@SpringBootTest
+class MyTest @Autowired constructor(private val orgSvc: IOrgService) {
+
+    @Test
+    fun t() {
+        val tree = orgSvc.getTree("")
+        tree.subscribe(::println)
+        tree.blockLast()
+        throw RuntimeException("abc")
+    }
+}

bc6924a9648f5e7ba7dc03707bab20666a2606b5
cuitao <cuitaocrazy@163.com>
2020-02-24 12:52:37 +0800

    少svc更新的处理

--- gw/src/main/kotlin/com/yada/scheduling/SchedulingConfig.kt
+++ gw/src/main/kotlin/com/yada/scheduling/SchedulingConfig.kt
@@ -2,11 +2,13 @@ package com.yada.scheduling
 
 import com.yada.gateways.SvcRoutePredicateFactory
 import com.yada.model.Res
+import com.yada.model.Svc
+import com.yada.services.ISvcService
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.autoconfigure.AutoConfigureBefore
 import org.springframework.cloud.gateway.config.GatewayAutoConfiguration
 import org.springframework.cloud.gateway.config.GatewayProperties
 import org.springframework.cloud.gateway.support.ConfigurationService
-import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.core.env.Environment
 import org.springframework.http.MediaType
@@ -20,12 +22,15 @@ import reactor.core.publisher.Mono
 @Configuration
 @EnableScheduling
 @AutoConfigureBefore(GatewayAutoConfiguration::class)
-open class GwConfig {
+open class GwConfig @Autowired constructor(
+        properties: GatewayProperties,
+        svcRoutePredicateFactory: SvcRoutePredicateFactory,
+        configurationService: ConfigurationService,
+        env: Environment,
+        private val svcService: ISvcService) {
     private val svcMap = mutableMapOf<String, String>()
 
-    @Bean
-    open fun svcSet(properties: GatewayProperties, svcRoutePredicateFactory: SvcRoutePredicateFactory, configurationService: ConfigurationService, env: Environment): MutableMap<String, String> {
-        svcMap.clear()
+    init {
         val list = properties.routes.map { routeDefinition ->
             val appPredicate = routeDefinition.predicates.filter { it.name == "Svc" }.firstOrNull()
             if (appPredicate != null) {
@@ -36,23 +41,17 @@ open class GwConfig {
                 Pair(config.svcId, "$schema://localhost:$port" + UriComponentsBuilder.fromPath(config.pathPrefix).pathSegment(config.svcId).pathSegment("res_list").encode().build().toUriString())
             } else null
         }.filterNotNull()
-
         list.forEach {
             svcMap[it.first] = it.second
         }
-
-        return svcMap
     }
 
-    @Scheduled(fixedDelay = 1000, initialDelay = 10000)
+    @Scheduled(fixedDelay = 1 * 60 * 60 * 1000, initialDelay = 10000)
     fun fetchServiceRes() {
-        println(svcMap)
         svcMap.forEach {
-            val str: Mono<List<Res>> = WebClient.create(it.value).get().accept(MediaType.APPLICATION_JSON).retrieve().bodyToMono()
-            str.subscribe {
-                // TODO 需要改测试数据，没有save，明天再说
-                println(it)
-            }
+            WebClient.create(it.value).get().accept(MediaType.APPLICATION_JSON).retrieve().bodyToMono<List<Res>>().flatMap { resList ->
+                svcService.createOrUpdate(Svc(it.key, resList.toSet()))
+            }.subscribe()
         }
     }
 }

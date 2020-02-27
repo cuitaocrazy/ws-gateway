0525f7193f4add42d6b33acbc56b7e07ebbcb0f0
cuitao <cuitaocrazy@163.com>
2020-02-27 23:55:16 +0800

    添加问题注释

--- gw/src/main/kotlin/com/yada/scheduling/SchedulingConfig.kt
+++ gw/src/main/kotlin/com/yada/scheduling/SchedulingConfig.kt
@@ -18,6 +18,20 @@ import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.reactive.function.client.bodyToMono
 import org.springframework.web.util.UriComponentsBuilder
 
+/**
+ *
+ * # service资源列表更新任务
+ *
+ * ## 关于动态更新路由的一些问题(目前不支持)
+ *
+ * 目前只能注入[ConfigurationService]来准确的获取svcId，不能从[RouteLocator][org.springframework.cloud.gateway.route.RouteLocator]
+ * 读取[SvcRoutePredicateFactory.Config], 因此就算写一个[ApplicationListener][org.springframework.context.ApplicationListener]去监听
+ * [RefreshRoutesEvent][org.springframework.cloud.gateway.event.RefreshRoutesEvent], 也不能读取[SvcRoutePredicateFactory.Config]，
+ *
+ * 参考[GatewayControllerEndpoint.routes][org.springframework.cloud.gateway.actuate.GatewayControllerEndpoint.routes]方法，它现在
+ * 也没实现这个问题，但是他有TODO等待完成，因此等待它去完善这个再去实现动态更新。
+ *
+ */
 @Configuration
 @EnableScheduling
 @AutoConfigureBefore(GatewayAutoConfiguration::class)
@@ -30,16 +44,17 @@ open class GwConfig @Autowired constructor(
     private val svcMap = mutableMapOf<String, String>()
 
     init {
-        val list = properties.routes.map { routeDefinition ->
-            val appPredicate = routeDefinition.predicates.filter { it.name == "Svc" }.firstOrNull()
-            if (appPredicate != null) {
-                // org.springframework.boot.web.serve.AbstractConfigurableWebServerFactory默认端口：8080
-                val port = env.getProperty("server.port") ?: "8080"
-                val schema = if (env.getProperty("server.ssl.key-store") == null) "http" else "https"
-                val config = configurationService.with(svcRoutePredicateFactory).name(appPredicate.name).properties(appPredicate.args).bind() as SvcRoutePredicateFactory.Config
-                Pair(config.svcId, "$schema://localhost:$port" + UriComponentsBuilder.fromPath(config.pathPrefix).pathSegment(config.svcId).pathSegment("res_list").encode().build().toUriString())
-            } else null
-        }.filterNotNull()
+        val list = // org.springframework.boot.web.serve.AbstractConfigurableWebServerFactory默认端口：8080
+                properties.routes.mapNotNull { routeDefinition ->
+                    val appPredicate = routeDefinition.predicates.filter { it.name == "Svc" }.firstOrNull()
+                    if (appPredicate != null) {
+                        // org.springframework.boot.web.serve.AbstractConfigurableWebServerFactory默认端口：8080
+                        val port = env.getProperty("server.port") ?: "8080"
+                        val schema = if (env.getProperty("server.ssl.key-store") == null) "http" else "https"
+                        val config = configurationService.with(svcRoutePredicateFactory).name(appPredicate.name).properties(appPredicate.args).bind() as SvcRoutePredicateFactory.Config
+                        Pair(config.svcId, "$schema://localhost:$port" + UriComponentsBuilder.fromPath(config.pathPrefix).pathSegment(config.svcId).pathSegment("res_list").encode().build().toUriString())
+                    } else null
+                }
         list.forEach {
             svcMap[it.first] = it.second
         }

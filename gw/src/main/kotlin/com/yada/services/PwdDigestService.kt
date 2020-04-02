05db8b2ce0d82ab1457e63dec3b6bb77f1a1769f
lirenhao <renhao.li@bjyada.com>
2020-04-02 17:23:09 +0800

    将用户默认密码改为可配置

--- gw/src/main/kotlin/com/yada/services/PwdDigestService.kt
+++ gw/src/main/kotlin/com/yada/services/PwdDigestService.kt
@@ -2,6 +2,8 @@ package com.yada.services
 
 import org.apache.commons.codec.binary.Base64
 import org.apache.commons.codec.digest.DigestUtils
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 
 interface IPwdDigestService {
@@ -10,8 +12,8 @@ interface IPwdDigestService {
 }
 
 @Service
-class PwdDigestService : IPwdDigestService {
-    override fun getDefaultPwdDigest(username: String): String = getPwdDigest(username, "changepwd")
+class PwdDigestService @Autowired constructor(@Value("\${yada.user.defaultPwd:changepwd}") private val defaultPwd: String) : IPwdDigestService {
+    override fun getDefaultPwdDigest(username: String): String = getPwdDigest(username, defaultPwd)
 
     override fun getPwdDigest(username: String, pwdPlaintext: String): String = Base64.encodeBase64String(DigestUtils.sha1(username + pwdPlaintext))
 }

8119e13b47c284e1a9167d91f56031a4fd2b18aa
cuitao <cuitaocrazy@163.com>
2020-06-28 16:31:04 +0800

    修改上下文

--- gw/src/main/kotlin/com/yada/web/routers/AdminAuthRouterConfig.kt
+++ gw/src/main/kotlin/com/yada/web/routers/AdminAuthRouterConfig.kt
@@ -12,6 +12,7 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.reactive.function.server.body
 import org.springframework.web.reactive.function.server.router
+import reactor.core.publisher.Mono
 
 @Configuration
 open class AdminAuthRouterConfig @Autowired constructor(
@@ -33,7 +34,7 @@ open class AdminAuthRouterConfig @Autowired constructor(
             POST("/apis/change_pwd", adminAuthHandler::changePwd)
             GET("/apis/refresh_token", adminAuthHandler::refreshToken)
             GET("/ui") {
-                ok().body(AuthHolder.getUserInfo())
+                AuthHolder.getUserInfo().flatMap { ok().body(Mono.just(it)) }
             }
             filter(whitelistFilter)
             filter(authApiHandlerFilter)
--- gw/src/main/kotlin/com/yada/web/routers/AuthRouterConfig.kt
+++ gw/src/main/kotlin/com/yada/web/routers/AuthRouterConfig.kt
@@ -12,6 +12,7 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.web.reactive.function.server.ServerResponse
 import org.springframework.web.reactive.function.server.body
 import org.springframework.web.reactive.function.server.router
+import reactor.core.publisher.Mono
 
 @Configuration
 open class AuthRouterConfig @Autowired constructor(
@@ -42,7 +43,7 @@ open class AuthRouterConfig @Autowired constructor(
             GET("/refresh_token", authHandler::refreshToken)
             GET("/filter_apis", authHandler::filterApis)
             GET("/ui") {
-                ok().body(AuthHolder.getUserInfo())
+                AuthHolder.getUserInfo().flatMap { ok().body(Mono.just(it)) }
             }
             filter(authApiFilter)
         }
